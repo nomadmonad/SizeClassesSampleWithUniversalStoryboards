e0a16d46f24da2972a3fc7e99e67bae83a250d39
Tsuyoshi Yamamoto <tsuyoshi.yamamoto@jrits.ricoh.co.jp>
2014-12-12 09:48:50 +0900

    initial Commit

---
+++ SizeClassesSampleWithUniversalStoryboards/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SizeClassesSampleWithUniversalStoryboards
+//
+//  Created by tyamamo on 2014/12/12.
+//  Copyright (c) 2014年 private. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SizeClassesSampleWithUniversalStoryboardsTests/SizeClassesSampleWithUniversalStoryboardsTests.m
@@ -0,0 +1,40 @@
+//
+//  SizeClassesSampleWithUniversalStoryboardsTests.m
+//  SizeClassesSampleWithUniversalStoryboardsTests
+//
+//  Created by tyamamo on 2014/12/12.
+//  Copyright (c) 2014年 private. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SizeClassesSampleWithUniversalStoryboardsTests : XCTestCase
+
+@end
+
+@implementation SizeClassesSampleWithUniversalStoryboardsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
